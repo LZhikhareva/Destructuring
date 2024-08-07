0dedb1eaffb4fc0dff76637a9bde7906b0182641
Liza <lisalisalisa57@mail.ru>
2024-08-07 09:34:59 +0300

    small changes

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,4 +1,4 @@
-import { character, special, getInfo } from '../app';
+import { special, getInfo } from '../app';
 
 test('check if getInfo works correct', () => {
   expect(getInfo(special)).toEqual([[8, "Двойной выстрел", "http://...", "Двойной выстрел наносит двойной урон"], [9, "Нокаутирующий удар", "http://...", "Описание недоступно"]]);
